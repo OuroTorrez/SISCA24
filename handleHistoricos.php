7747bf401faf2053a97e01c55de4c3c598d3e224
Mauricio T.B. <113544064+MauricioTB97@users.noreply.github.com>
2024-08-12 12:34:43 -0600

    reporte historicos

---
+++ handleHistoricos.php
@@ -0,0 +1,251 @@
+<?php
+include 'conexion.php';
+// Establish database connection
+$conn = conectar();
+if (empty($conn) || !($conn instanceof mysqli)) {
+    $error = "⛔Error de conexión: <br>" . $conn;
+}
+?>
+<?php if (isset($error)) { ?>
+    <div id="Errores">
+        <div id="Error">
+            <p><?php echo $error; ?></p>
+        </div>
+    </div>
+<?php } ?>
+
+<?php
+if ($_POST['accion'] == "showHistoricos") {
+    if (isset($_POST['almacen'])) {
+        $almacen = $_POST['almacen'];
+        $mes = $_POST['mes'];
+        $anio = $_POST['anio'];
+        $mesant = $mes - 1;
+        if($mesant == 0){
+            $mesant = 12;
+            $anio = $anio - 1;
+        }
+    } else {
+        $almacen = 0;
+    }
+    if ($almacen != 0) {
+        $query = $conn->prepare("SELECT d.clave, d.programa, d.producto, d.medida,
+            -- Calculo de existencias mes anterior
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0
+                AND rd.id_almacen = ? 
+                AND MONTH(rd.fecha_registro) = $mesant 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) 
+            - COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0
+                AND sd.id_almacen = ? 
+                AND MONTH(sd.fecha_registro) = $mesant 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+            ) AS existencias_ant,
+            -- Cantidad de entradas durante el mes y año especificado
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0
+                AND rd.id_almacen = ? 
+                AND MONTH(rd.fecha_registro) = $mes 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) AS entradas,
+
+            -- Cantidad de salidas durante el mes y año especificado
+            COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0
+                AND sd.id_almacen = ? 
+                AND MONTH(sd.fecha_registro) = $mes 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+            ) AS salidas,
+
+            -- Cálculo de existencias
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0
+                AND rd.id_almacen = ? 
+                AND MONTH(rd.fecha_registro) = $mes 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) 
+            - COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0
+                AND sd.id_almacen = ? 
+                AND MONTH(sd.fecha_registro) = $mes 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+        ) AS existencias
+        FROM dotaciones d");
+
+        $query->bind_param("iiiiii", $almacen, $almacen, $almacen, $almacen, $almacen, $almacen);
+    } else {
+        $query = $conn->prepare("SELECT 
+            d.clave, 
+            d.programa, 
+            d.producto, 
+            d.medida, 
+            -- Calculo de existencias mes anterior
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0 
+                AND MONTH(rd.fecha_registro) = $mesant 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) 
+            - COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0 
+                AND MONTH(sd.fecha_registro) = $mesant 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+            ) AS existencias_ant,
+            -- Cantidad de entradas durante el mes y año especificado
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0 
+                AND MONTH(rd.fecha_registro) = $mes 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) AS entradas,
+
+            -- Cantidad de salidas durante el mes y año especificado
+            COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0 
+                AND MONTH(sd.fecha_registro) = $mes 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+            ) AS salidas,
+
+            -- Cálculo de existencias
+            COALESCE(
+                (SELECT SUM(dr.cantidad) 
+                FROM registro_entradas_registradas dr 
+                INNER JOIN registro_entradas rd ON dr.folio = rd.folio 
+                WHERE dr.clave = d.clave 
+                AND rd.cancelado = 0 
+                AND MONTH(rd.fecha_registro) = $mes 
+                AND YEAR(rd.fecha_registro) = $anio), 
+                0
+            ) 
+            - COALESCE(
+                (SELECT SUM(sr.cantidad) 
+                FROM registro_salidas_registradas sr 
+                INNER JOIN registro_salidas sd ON sr.folio = sd.folio 
+                WHERE sr.clave = d.clave 
+                AND sd.cancelado = 0 
+                AND MONTH(sd.fecha_registro) = $mes 
+                AND YEAR(sd.fecha_registro) = $anio), 
+                0
+            ) AS existencias
+
+        FROM dotaciones d;");
+        if ($query === false) {
+            die('Error en la consulta SQL: ' . $conn->error);
+        }
+    }
+    if ($query->execute()) {
+        $query->bind_result($clave, $programa, $producto, $medida, $existencias_ant, $entradas , $salidas, $existencias);
+        $query->store_result();
+        ?>
+        <table class="tablaHistoricos">
+            <tr>
+                <th colspan='8' class='ProgramaTitle'>Personas Adultas Mayores</th>
+            </tr>
+            <tr>
+                <th style="width: 15%;">Clave</th>
+                <th style="width: 40%;">Producto</th>
+                <th style="width: 15%;">Medida</th>
+                <!---->
+                <th style="width: 10%;">Existencias Anteriores</th>
+                <th style="width: 10%;">Entradas</th>
+                <th style="width: 10%;">Salidas</th>
+                <!---->
+                <th style="width: 10%;">Existencias</th>
+            </tr>
+            <?php
+            $previousPrograma = "Personas Adultas Mayores";
+            if ($query->num_rows > 0) {
+                while ($query->fetch()) {
+                    if ($programa != $previousPrograma) {
+                        ?>
+                    </table>
+                    <table class="tablaHistoricos">
+                        <tr>
+                            <th colspan='8' class='ProgramaTitle'><?php echo $programa ?></th>
+                        </tr>
+                        <tr>
+                            <th style="width: 15%;">Clave</th>
+                            <th style="width: 40%;">Producto</th>
+                            <th style="width: 15%;">Medida</th>
+                            <!---->
+                            <th style="width: 10%;">Existencias Anteriores</th>
+                            <th style="width: 10%;">Entradas</th>
+                            <th style="width: 10%;">Salidas</th>
+                            <!---->
+                            <th style="width: 10%;">Existencias</th>
+                        </tr>
+                        <?php
+                        $previousPrograma = $programa;
+                    }
+                    echo "<tr>";
+                    echo "<td>$clave</td>";
+                    echo "<td>$producto</td>";
+                    echo "<td>$medida</td>";
+                    //
+                    echo "<td>$existencias_ant</td>";
+                    echo "<td>$entradas</td>";
+                    echo "<td>$salidas</td>";
+                    //
+                    echo "<td>$existencias</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<p>No hay existencias historicas</p>";
+            }
+    } else {
+        echo "<p>Error al r las existencias historicas</p>";
+    }
+    ?>
+    </table>
+    <?php
+}
--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
-    // Si no hay una sesión iniciada, iniciarla
-    if(session_status() == PHP_SESSION_NONE){
-        session_start();
-    }
-    // Si no hay un usuario loggeado, redirigir a la página de login
-    if(!isset($_SESSION['usuario']) || !$_SESSION['LoggedIn']){
-        header('Location: login.php');
-    }
+// Si no hay una sesión iniciada, iniciarla
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+// Si no hay un usuario loggeado, redirigir a la página de login
+if (!isset($_SESSION['usuario']) || !$_SESSION['LoggedIn']) {
+    header('Location: login.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -28,9 +28,9 @@
             <!-- Header menu de navegación -->
             <ul class="HeaderMenuNav">
                 <li class="MenuNavOption"><a href="index.php">Inicio</a></li><!-- Boton de incio -->
-                
-                
-                <?php if(isset($_SESSION['rol']) && ($_SESSION['rol'] == 3 || $_SESSION['rol'] == 4 || $_SESSION['rol'] == 5 || $_SESSION['rol'] == 6 || $_SESSION['rol'] == 7)) { ?><!-- Menu para coordinador administrativo y control de almacenes -->
+
+
+                <?php if (isset($_SESSION['rol']) && ($_SESSION['rol'] == 3 || $_SESSION['rol'] == 4 || $_SESSION['rol'] == 5 || $_SESSION['rol'] == 6 || $_SESSION['rol'] == 7)) { ?><!-- Menu para coordinador administrativo y control de almacenes -->
                     <li class="MenuNavOption"><a href="consultas.php">Entradas</a><!-- Boton de entradas coordinador -->
                         <ul class="HeaderMenuSubNav">
                             <li class="MenuNavOption"><a href="consultas.php">Consulta</a></li>
@@ -43,7 +43,7 @@
                     </li>
 
 
-                <?php } else {?> <!-- Menu para almacenista, usuario común -->
+                <?php } else { ?> <!-- Menu para almacenista, usuario común -->
                     <li class="MenuNavOption"><a href="entradas.php">Entradas</a><!-- Boton de entradas -->
                         <ul class="HeaderMenuSubNav">
                             <li class="MenuNavOption"><a href="entradas.php">Captura</a></li><!-- Submenu de captura de entradas -->
@@ -59,20 +59,25 @@
                         </ul>
                     </li>
                 <?php } ?>
-                <li class="MenuNavOption"><a href="existencias.php">Existencias</a></li><!-- Boton de existencias -->
+                <li class="MenuNavOption"><a href="existencias.php">Existencias</a><!-- Boton de existencias -->
+                    <ul class="HeaderMenuSubNav">
+                        <li class="MenuNavOption"><a href="existencias.php">Actuales</a></li><!-- Submenu de existencias -->
+                        <hr>
+                        <li class="MenuNavOption"><a href="historicos.php">Historicos</a></li><!-- Submenu de existencias -->
+                    </ul>
+                </li>
 
 
-                <?php if(isset($_SESSION['rol']) && $_SESSION['rol'] == 1){ ?> <!-- Opción solo para administradores -->
-                <li class="MenuNavOption"><a href="controlUsuarios.php">Usuarios</a></li> <!-- Boton de registro y modificación de usuarios -->
+                <?php if (isset($_SESSION['rol']) && $_SESSION['rol'] == 1) { ?> <!-- Opción solo para administradores -->
+                    <li class="MenuNavOption"><a href="controlUsuarios.php">Usuarios</a></li> <!-- Boton de registro y modificación de usuarios -->
                 <?php } ?>
             </ul>
         </div>
         <!-- Header cuenta Menú de usuario -->
         <div id="HeaderAccount">
-            <div id="HeaderAccountButton"><img id="HeaderAccountImage" src="Media/AccountIconBlanco.png"
-                    alt="Imagen de perfil"></div>
+            <div id="HeaderAccountButton"><img id="HeaderAccountImage" src="Media/AccountIconBlanco.png" alt="Imagen de perfil"></div>
             <div id="HeaderAccountMenu">
-                <span style="text-align: center; text-transform: uppercase; padding: 15px 20px; font-size: larger;"><?php echo $_SESSION ['usuario']?></span>
+                <span style="text-align: center; text-transform: uppercase; padding: 15px 20px; font-size: larger;"><?php echo $_SESSION['usuario'] ?></span>
                 <hr>
                 <a href="perfil.php"><i class="bi bi-person-fill"></i> Mi perfil</a>
                 <hr>
@@ -84,8 +89,8 @@
 
 </html>
 <script>
-// Boton clickeable de la cuenta
-document.getElementById('HeaderAccountButton').addEventListener('click', function() {
-    document.getElementById('HeaderAccountMenu').classList.toggle('show');
-});
+    // Boton clickeable de la cuenta
+    document.getElementById('HeaderAccountButton').addEventListener('click', function() {
+        document.getElementById('HeaderAccountMenu').classList.toggle('show');
+    });
 </script>
